93296b659d57f3e25cf6247e88833f8be489d157
YL_Glotz <yuexinli@berkeley.edu>
2020-12-01 20:30:37 -0800

    add parallel

--- scihub_download_s1_sar_parallel.sh
+++ scihub_download_s1_sar_parallel.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # Download data as shown in the search_results. 
-# Feb. 14, 2018.
+# Feb. 14, 2018. by Kathryn Materna
+#
+# Last updated Dec 2020
+# By YL
+# Incorporate parallel
+
 
 if [[ "$#" -eq 0 ]]; then
   echo ""
   echo "This script downloads the results of data queries"
-  echo "Usage: ./scihub_download_s1_sar.sh -options"
-  echo "Example: ./scihub_download_s1_sar.sh -i search_results1.txt -i search_results2.txt"
-  echo "Please provide one or more input files."
+  echo "Usage: ./scihub_download_s1_sar_parallel.sh -options"
+  echo "Example: ./scihub_download_s1_sar.sh -i search_results.txt"
+  echo "Current parallel version only works for single input file."
   echo ""
   exit 1
 fi
 
+# Remove command files
+rm -f download_s1a_parallel.cmd
 
 # Read the search results. It could be multiple calls of the -i flag. 
 while getopts i: opt; do
@@ -27,10 +34,8 @@ shift $((OPTIND -1))
 # Defining parameters
 id_results=uuid_file.txt
 
-
 #Where will the data live? 
 mkdir -p DATA
-#mkdir -p MANIFEST
 
 
 # WILL FIX THIS TO CAT ANY DATA IN ANY FILE
@@ -67,15 +72,25 @@ while read p; do
   elif [ -f  DATA/"$title".SAFE.zip ]; then
     echo "Zip File exists. Skip downloading process."
   else
-    wget -c --no-check-certificate --http-user=yuexinli@berkeley.edu  --http-password=Give_me_5  -O DATA/"$title".zip "https://datapool.asf.alaska.edu/SLC/SA/$title.zip"
+    # make command list for all jobs
+    echo "wget -c --no-check-certificate --http-user=yuexinli@berkeley.edu  --http-password=Give_me_5  -O DATA/"$title".zip "https://datapool.asf.alaska.edu/SLC/SA/$title.zip"" >> download_s1a_parallel.cmd 
 
   # Takes a few hours for each SAFE.zip.
   # Each one can be unzipped with unzip.
-  unzip DATA/$title.zip
-  rm DATA/$title.zip
+  #unzip DATA/$title.zip
+  #rm DATA/$title.zip
   
   fi
 
   counter=0
-  
-done <$id_results
+
+done < $id_results  
+
+# Currently ncores is hard-coded to be 4
+  t1=`date`
+  parallel --jobs 4 < download_s1a_parallel.cmd
+  t2=`date`
+  #echo $t1,$t2
+  echo "Downloading job started on $t1 and finished on $t2." | mail -s "Sentinel Download Finished" yuexinli@berkeley.edu
+
+
